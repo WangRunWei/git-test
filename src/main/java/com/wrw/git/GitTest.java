125b76ea45f7c6cf17506188c2e1c1bca57da5d5
Wang <2629424879@qq.com>
2023-11-18 19:10:01 +0800

    hot-fix commit2

--- src/main/java/com/wrw/git/GitTest.java
+++ src/main/java/com/wrw/git/GitTest.java
@@ -7,7 +7,7 @@ package com.wrw.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println(11111);
-        System.out.println(11111);
+        System.out.println(22222);
         System.out.println(22222);
     }
     
