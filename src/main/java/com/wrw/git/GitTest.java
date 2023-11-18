0323556e6dbbab9b37777595429decec3f098be8
Wang <2629424879@qq.com>
2023-11-18 19:06:15 +0800

    hot-fix commit1

---
+++ src/main/java/com/wrw/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wrw.git;
+
+/**
+ * @author 王润伟
+ * @date 2023年11月18日 18:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(11111);
+        System.out.println(11111);
+    }
+    
+}
